3506a05ee936a0860189d412420c60f8fa090b16
Nikita Dobrynin <widauka@ya.ru>
2024-09-28 00:45:54 +0500

    Reading a config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{env, fs};
+
+use toml::Table;
+
 fn main() {
-    println!("Hello, world!");
+    let config_path = format!("{}/.config/looper/schedule.toml", env::var("HOME").unwrap());
+    let config = fs::read_to_string(&config_path)
+        .expect(format!("No configuration file at {}", &config_path).as_str());
+    println!("{}", config);
+    // let config = "foo = 'bar'".parse::<Table>().unwrap();
+    // println!("foo is {}", config["foo"].as_str().unwrap());
 }
