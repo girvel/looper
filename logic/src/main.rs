e1de40e30bca86ce469f0a3862092e0bc27171a7
Nikita Dobrynin <widauka@ya.ru>
2024-09-28 17:55:23 +0500

    Display some info on `looper done`

--- logic/src/main.rs
+++ logic/src/main.rs
@@ -7,14 +7,20 @@ use heavy::{parse_cli, read_schedule, read_state, write_state, Commands, Routine
  *
  * - Install as executable
  * - Publish
- * - Redo schedule as a hashmap
- * - Display message on done
+ * x Redo schedule as a hashmap
+ * x Display message on done
+ * - README
+ * - Help message
+ * - 1.0!
+ * - dotfile
  */
 
 struct App {
     schedule: HashMap<String, Routine>,
     state: State,
 }
+
+const DATE_FORMAT: &str = "%d-%b-%Y";
 
 impl App {
     fn new() -> Self {
@@ -24,27 +30,30 @@ impl App {
         }
     }
 
+    fn get_next_date(&self, id: &str) -> Option<DateTime<Local>> {
+        let period = self.schedule.get(id)?.period.as_deref()?;
+        Some(cron::Schedule::from_str(period)
+            .unwrap()
+            .after(
+                self.state.finish_times
+                    .get(id)
+                    .unwrap_or(&DateTime::<Local>::default())
+            )
+            .next()
+            .unwrap())
+    }
+
     fn show(&self) {
         let mut schedule_table: Vec<_> = self.schedule.iter()
             .filter_map(|(id, routine)| {
-                let period = routine.period.as_deref()?;
-                let time = cron::Schedule::from_str(&period)
-                    .unwrap()
-                    .after(
-                        self.state.finish_times
-                            .get(id)
-                            .unwrap_or(&DateTime::<Local>::default())
-                    )
-                    .next()
-                    .unwrap();
-
+                let time = self.get_next_date(id)?;
                 Some((id, &routine.name, time))
             })
             .collect();
 
         schedule_table.sort_by_key(|&(_, _, time)| Reverse(time));
 
-        println!("\n{}", format!("Today is {}:", Local::now().format("%d-%b-%Y")).bright_white());
+        println!("\n{}", format!("Today is {}:", Local::now().format(DATE_FORMAT)).bright_white());
         loop {
             if schedule_table.last().map_or(true, |&(_, _, t)| t > Local::now()) { break; }
             let (id, name, _) = schedule_table.pop().unwrap();
@@ -53,7 +62,6 @@ impl App {
                 "{}  {}",
                 format!("#{}", id).bright_black(),
                 name,
-                // format!("@{}", time.format("%d-%b-%Y")).bright_black(),
             );
         }
 
@@ -64,30 +72,34 @@ impl App {
                 "{}  {}  {}",
                 format!("#{}", id).bright_black(),
                 name,
-                format!("@{}", time.format("%d-%b-%Y")).bright_black(),
+                format!("@{}", time.format(DATE_FORMAT)).bright_black(),
             );
         }
     }
 
     fn done(&mut self, routine_id: &str) {
-        let period = &self.schedule.get(routine_id)
-            .unwrap_or_else(|| panic!("Unable to find a task with id {}", routine_id))
-            .period.as_ref()
+        let routine = &self.schedule.get(routine_id)
+            .unwrap_or_else(|| panic!("Unable to find a task with id {}", routine_id));
+
+        let period = routine.period.as_ref()
             .unwrap_or_else(|| panic!("No period specified for task with id {}", routine_id));
 
-        self.state.finish_times.insert(
-            routine_id.to_string(),
-            if let Some(finish_time) = self.state.finish_times.get(routine_id) {
-                cron::Schedule::from_str(period)
-                    .unwrap()
-                    .after(finish_time)
-                    .next()
-                    .unwrap()
-            } else {
-                Local::now()
-            }
-        );
+        let new_finish_time = if let Some(finish_time) = self.state.finish_times.get(routine_id) {
+            cron::Schedule::from_str(period)
+                .unwrap()
+                .after(finish_time)
+                .next()
+                .unwrap()
+        } else {
+            Local::now()
+        };
+
+        self.state.finish_times.insert(routine_id.to_string(), new_finish_time.clone());
         write_state(&self.state);
+
+        println!("\n{}", routine.name.bright_white());
+        println!("Done @{}", new_finish_time.format(DATE_FORMAT));
+        println!("Next @{}", self.get_next_date(routine_id).unwrap().format(DATE_FORMAT));
     }
 }
 
